4e5afe070ff55bdc6aba677343a78087b7834586
chethanat <56339319+chethanat@users.noreply.github.com>
2024-04-01 14:22:19 +0530

    Create maven.py

---
+++ maven.py
@@ -0,0 +1,17 @@
+import yaml
+import subprocess
+
+
+def execute_maven(pipeline):
+    if pipeline:
+        subprocess.run(['mvn', 'clean'])
+    else:
+        print("Pipeline parameter is set to False")
+
+
+if __name__ == '__main__':
+    with open('test.yml', 'r') as  yaml_file:
+        data = yaml.safe_load(yaml_file)
+
+    pipeline = data.get('test', {}).get('pipeline', False)
+    execute_maven(pipeline)
